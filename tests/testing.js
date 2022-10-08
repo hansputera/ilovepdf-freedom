f61dd2f95c8ea8ab40155dd4caf158619560b0ad
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-10-08 06:59:40 +0000

    feat(tests): await the function if identified as AsyncFunction
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- tests/testing.js
+++ tests/testing.js
@@ -1,17 +1,30 @@
 const {format} = require('node:util');
 
 /**
+ * Check the function is async or not
+ * @param {*} data Any data
+ * @return {boolean}
+ */
+const isAsyncFunc = (data) => data && data.constructor.name === 'AsyncFunction';
+/**
  *
  * @param {string} name Test name
  * @param {Function} func Test function.
  */
 const doTest = async (name, func) => {
   try {
-    func();
+    if (isAsyncFunc(func)) {
+      await func();
+      success = true;
+    } else {
+      func();
+      success = true;
+    }
 
-    console.log(format('[%s]: success', name));
+    if (success) console.log(format('[%s]: success', name));
+    else console.log(format('[%s]: fail', name));
   } catch (e) {
-    console.log(format('[%s]: %s fail', name, e?.message));
+    console.log(format("[%s]: fail cuz '%s'", name, e?.message));
   }
 };
 
