87a544331dac0866d79a974a3990d2df8ab71d85
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-10-08 07:04:09 +0000

    feat(@typings): add Task.process return types
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/@typings/index.ts
+++ src/@typings/index.ts
@@ -14,4 +14,23 @@ export type Tool =
   | 'pdfa'
   | 'htmlpdf';
 
+export interface IDownloadProcessed {
+  download_filename: string;
+  filesize: number;
+  output_filesize: number;
+  output_filenumber: number;
+  output_extensions: string;
+  timer: string;
+  status: string;
+}
+
+// ref: https://developer.ilovepdf.com/docs/api-reference#errors
+export interface IError {
+  type: string;
+  message?: string;
+  param?: IParamError;
+  code?: number;
+}
+export type IParamError = Record<string, string | string[]>;
+
 export * from './config';
